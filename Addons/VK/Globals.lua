ed0c429b1f55a9fc4d15c0d87c42555f4e0637e7
vulcan-dev <vitexuhd@gmail.com>
2021-09-29 12:18:21 +0100

    Added File Logging

--- Addons/VK/Globals.lua
+++ Addons/VK/Globals.lua
@@ -1,10 +1,27 @@
 --[[ Logging Utilities ]]--
-function GLog(message, ...) print(string.format('[%s]  [kissmp_server:vk] %s', DateTime(), string.format(message, ...))) end
-function GDLog(message, ...) message = tostring(string.format('[ DEBUG]: %s', message)) GLog(message, ...) end
-function GILog(message, ...) message = tostring(string.format('[ INFO]: %s', message)) GLog(message, ...) end
-function GWLog(message, ...) message = tostring(string.format('[ WARN]: %s', message)) GLog(message, ...) end
-function GELog(message, ...) message = tostring(string.format('[ ERROR]: %s', message)) GLog(message, ...) end
-function GFLog(message, ...) message = tostring(string.format('[ FATAL]: %s', message)) GLog(message, ...) end
+function GWriteToFile(path, ...)
+    local file = io.open(path, 'a')
+    if file then
+        file:write(...)
+        file:close()
+    else
+        GELog('Failed Writing to File: %s', path)
+    end
+end
+
+function GLog(message, level, ...)
+    if level >= GLevel then print(string.format('[%s]  [kissmp_server:vk] %s', DateTime(), string.format(message, ...))) end
+    if ... then message = message .. ... message = string.format(message, ...) end
+    message = message .. '\n'
+
+    if GFileLogging and level >= GLogFileLevel then GWriteToFile(string.format('Addons\\VK\\Server\\Logs\\%s\\%s', GLogFilePath, GLogFile), tostring(message)) end
+end
+
+function GDLog(message, ...) message = tostring(string.format('[ DEBUG]: %s', message)) GLog(message, GLevels['Debug'], ...) end
+function GILog(message, ...) message = tostring(string.format('[ INFO]: %s', message)) GLog(message, GLevels['Info'], ...) end
+function GWLog(message, ...) message = tostring(string.format('[ WARN]: %s', message)) GLog(message, GLevels['Warn'], ...) end
+function GELog(message, ...) message = tostring(string.format('[ ERROR]: %s', message)) GLog(message, GLevels['Error'], ...) end
+function GFLog(message, ...) message = tostring(string.format('[ FATAL]: %s', message)) GLog(message, GLevels['Fatal'], ...) os.execute('pause') os.exit(1) end
 
 --[[ Utility Functions ]]--
 function DeepCopy(orig)
@@ -23,10 +40,12 @@ function DeepCopy(orig)
 end
 
 function AddPath(path)
+    if string.find(package.path, path) then return end
     package.path = ';' .. path .. '/?.lua;' .. package.path
 end
 
 function DateTime(format, time) return os.date(format or '%H:%M:%S', time) end
+
 function Include(include)
     if type(include) == 'string' then
         if package.loaded[include] then package.loaded[include] = nil end
@@ -45,3 +64,19 @@ end
 --[[ Variables ]]--
 GClients = {}
 GCommands = {}
+GProduction = nil
+
+GLevels = {
+    Debug = 0,
+    Info = 1,
+    Warn = 2,
+    Error = 3,
+    Fatal = 4
+}
+
+GLevel = nil
+GLogLevel = nil
+GLogFileLevel = nil
+GLogFilePath = nil
+GLogFile = nil
+GStartupTime = nil
--- Addons/VK/Main.lua
+++ Addons/VK/Main.lua
@@ -6,6 +6,17 @@ Utilities = require('Addons.VK.Utilities')
 local _Extensions = {}
 
 local function Setup()
+    --[[ Setup Server Settings ]]--
+    local Server = Utilities.FileToJSON('Addons\\VK\\Settings\\Server.json')
+    GLevel = GLevels[Server['Logging']['DefaultLogLevel']]
+    GFileLogging = Server['Logging']['FileLogging']
+    GLogFileLevel = GLevels[Server['Logging']['DefaultFileLogLevel']]
+
+    --[[ Check if not running debug ]]--
+    GProduction = Server['Production']
+    if GProduction then GLogFilePath = 'Production' else GLogFilePath = 'Development' end
+    if GFileLogging then GLogFile = string.format('Development (%s).log', GStartupTime) end
+
     --[[ Load Extensions ]]--
     local Extensions = Utilities.FileToJSON('Addons\\VK\\Settings\\Extensions.json')['Extensions']
     for _, extension in ipairs(Extensions) do
@@ -23,12 +34,13 @@ end
 
 --[[ Local Functions ]]--
 local function Initialise()
+    GStartupTime = DateTime('%d-%m-%Y %H-%M-%S')
     Setup()
 
     -- Setup ReloadModules --
     hooks.register('OnStdIn', 'ReloadModules', function()
         GILog('Reloading Modules')
-        Initialise()
+        Setup()
     end)
 
     AddPath('Addons/VK')
@@ -36,3 +48,8 @@ local function Initialise()
 end
 
 Initialise()
+
+--[[ TODO ]]--
+-- /set <option> <value>
+--   example: /set prefix !
+--   example: /set cmod 155:255:255 (RGB)
