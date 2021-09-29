0d1e788916beaea7c5ef4f3aac49a0e15a5ce046
vulcan-dev <vitexuhd@gmail.com>
2021-09-29 01:09:44 +0100

    A_CRP_SHARED Template (Not Final)

---
+++ Addons/VK/Client/A_CRP_SHARED/Lua/GE/Extensions/A_CRP_SHARED.lua
@@ -0,0 +1,6 @@
+local M = {}
+
+M.Interface = require('') -- Shared
+M.Utilities = require('') -- Shared
+
+return M
---
+++ Addons/VK/Client/A_CRP_SHARED/Scripts/A_CRP_SHARED/ModScript.lua
@@ -0,0 +1,4 @@
+log('I', 'A_CRP_SHARED', 'Loaded')
+
+load('A_CRP_SHARED')
+registerCoreModule('A_CRP_SHARED')
