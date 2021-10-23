d0aab0cd01f4f2db3e5d43c3e2f298f224be5029
vulcan-dev <vitexuhd@gmail.com>
2021-10-23 20:58:10 +0100

    Added /remove_warn

--- Addons/VK/Server/Extensions/VK-Essentials/Commands/C_REMOVE_WARN.lua
+++ Addons/VK/Server/Extensions/VK-Essentials/Commands/C_REMOVE_WARN.lua
@@ -4,23 +4,23 @@ local VKUtilities = Include('Addons.VK.Server.Extensions.VK-Essentials.Utilities
 
 Command.Rank = VKUtilities.RankModerator
 Command.Category = VKUtilities.CategoryModeration
-Command.Description = 'Unbans a user'
-Command.Usage = 'unban <user>'
+Command.Description = 'Remove a warn from a client'
+Command.Usage = 'remove_warn <user> <id>'
 Command.Execute = function(executor, arguments)
     local client = Server.GetUser(arguments[1])
+    local id = arguments[2]
     
     if not client then return GErrorInvalidUser end
 
     if executor.GetRank() <= client.GetRank() then return GErrorCannotPerformUser end
 
-    local ban = client.GetBanned()
-    if not ban then return client.GetName() .. ' is not banned' end
+    local warns = client.GetKey('Warnings')
+    if warns[id] then
+        warns[id] = nil
+        client.EditKey('Warnings', warns)
+    end
 
-    local bans = client.GetKey('Bans')
-    bans[tostring(ban.ID)].Length = 0
-    client.EditKey('Bans', bans)
-
-    Server.DisplayDialogSuccess(executor, string.format('Successfully Unbanned %s', client.GetName()))
+    Server.DisplayDialogSuccess(executor, string.format('Successfully Removed Warn From: %s', client.GetName()))
 end
 
 return Command
